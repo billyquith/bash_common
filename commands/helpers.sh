24c2ffacc4cbde7e9fb43fb20f790e85a8cb788b
billyquith <chinbillybilbo@gmail.com>
2021-07-02 19:14:50 +0100

    bcup command added. - bash_common update.

--- commands/helpers.sh
+++ commands/helpers.sh
@@ -1,11 +1,4 @@
 
-# bash_common update
-function bcup {
-    pushd $BC_INSTALL_DIR
-    git pull
-    popd
-}
-
 # Make a tempfile name
 function tmpfile {
     echo $(mktemp /tmp/temp.$$.XXXXXX.tmp)
